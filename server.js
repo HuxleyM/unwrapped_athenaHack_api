6eb9b5f62e81d1330533828c09bdc559bfa5bdf1
Elizabeth Venner <elizabeth.venner@kurtgeiger.com>
2019-04-14 11:48:25 +0100

    change port to 80

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express        = require('express');
 const bodyParser     = require('body-parser');
 const app            = express();
 
-const port = 8000;
+const port = 80;
 
 require('./app/routes/index')(app, {});
 app.listen(port, () => {
